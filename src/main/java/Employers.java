d8f778a309a6ade0510c6f00591cb9d22172f10b
Krzysztof Repec <repeckrzysztof@gmail.com>
2018-01-13 23:56:47 +0100

    Second commit Delete 2 code lines

--- src/main/java/Employers.java
+++ src/main/java/Employers.java
@@ -6,10 +6,8 @@ public class Employers {
 
     Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/hotel","root","1234");
     PreparedStatement preparedStatement = connection.prepareStatement("select * from employers");
-  //  PreparedStatement preparedStatement1 = connection.prepareStatement("describe employers");
     ResultSet resultSet = preparedStatement.executeQuery();
-   // ResultSet resultSet1 = preparedStatement.executeQuery();
-
+ 
 
 
 
